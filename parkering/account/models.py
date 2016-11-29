a6f2fef5db52ffc3293a172c64be00da26fa7809
Axel Pennsäter <pennsater@gmail.com>
2016-11-29 12:02:46 +0100

    database models

--- parkering/account/models.py
+++ parkering/account/models.py
@@ -11,3 +11,18 @@ class User_data(models.Model):
     date_of_birth = models.DateField(blank="True", null=True)
     parking_number = models.CharField(max_length=20, blank="True", null=True)
     phone_number = models.CharField(max_length=20, blank="True", null=True)
+
+class Parking_space(models.Model):
+    number = models.IntegerField(unique=True)
+    owner = models.ForeignKey(User)
+    
+    
+class Booking(models.Model):
+    space = models.ForeignKey(Parking_space)
+    taken = models.BooleanField()
+    start_date = models.DateField()
+    stop_date = models.DateField()
+    
+    
+
+    
