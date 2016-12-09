60549deed8b41349292b2d9d651d437a782b9d3a
Axel Pennsäter <pennsater@gmail.com>
2016-12-09 13:42:04 +0100

    Can book parkingspace

---
+++ parkering/kombo_parking/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import include, url
+from . import views
+
+urlpatterns = [
+
+    url(r'^$', views.Calender, name='Calender'),
+    #~ url(r'^(?P<slug>[-w]+)/$', 'hepsi', name = "hepsiliste")
+    url(r'^bookthatspace/(?P<bookid>[\d]+)$', views.Bookthatspace, name='Bookthatspace'),
+    
+
+]
