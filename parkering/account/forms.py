f418c7cd197b2dd9b899e761e0e12b56ec0494e5
Max <max.arnstrom@gmail.com>
2016-12-14 16:07:38 +0100

    Fixed proper forms error handeling for both login and register user

--- parkering/account/forms.py
+++ parkering/account/forms.py
@@ -1,23 +1,11 @@
 from django import forms
-from account.models import User_data
+from account.models import User_data, Apartment_number
 from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django.contrib import auth
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import AuthenticationForm
-
-# dynamic form for extended user model
-class UserDataForm(forms.ModelForm):
-
-    class Meta:
-        model = User_data
-        fields = ('phone_number','apartment')
-
-        widgets = { 
-            'phone_number': forms.TextInput(attrs={'placeholder': 'Phone number'}),
-            'apartment': forms.TextInput(attrs={'placeholder': 'Apartment number'}),
-        }
-
+from django.core.validators import RegexValidator
 
 # dynamic loginform
 class LoginForm(forms.Form):
@@ -32,39 +20,126 @@ class LoginForm(forms.Form):
     # remove when no longer required
     # access to this but no validation error is displayed in html, with or without tags
     # can be accessed by print(<form>.errors) in views.py
+    #~ def clean_password(self):
+        #~ cleaned_username = self.cleaned_data['username']
+        #~ cleaned_password = self.cleaned_data['password']
+
+        #~ user = auth.authenticate(username = cleaned_username, password = cleaned_password)
+        #~ if not user:
+            #~ raise forms.ValidationError(u"Incorrect login, please try again or register an account.")
+        #~ return cleaned_password
+
     def clean_password(self):
-        #cleaned_data = self.cleaned_data
         cleaned_username = self.cleaned_data['username']
         cleaned_password = self.cleaned_data['password']
 
         user = auth.authenticate(username = cleaned_username, password = cleaned_password)
-        if not user:
-            raise forms.ValidationError("Incorrect login, please try again or register an account.")
+
+        if user is not None:
+            if not user.is_active:
+                raise forms.ValidationError(u"This account has been inactivated.")
+        else:
+            raise forms.ValidationError(u"Incorrect login, please try again or register an account.")
+
         return cleaned_password
+        
+#~ # dynamic register form
+#~ class RegisterForm(forms.ModelForm):
 
+    #~ class Meta:
+        #~ model = User
+        #~ fields = ['username', 'email', 'first_name', 'last_name', 'password']
 
-# dynamic register form
-class RegisterForm(forms.ModelForm):
+        #~ widgets = { 
+            #~ 'username': forms.TextInput(attrs={'placeholder': 'Username'}),
+            #~ 'password': forms.PasswordInput(attrs={'placeholder': 'Password'}),
+            #~ 'email': forms.EmailInput(attrs={'placeholder': 'E-mail address'}),
+            #~ 'first_name': forms.TextInput(attrs={'placeholder': 'first name'}),
+            #~ 'last_name': forms.TextInput(attrs={'placeholder': 'last name'}),
+         #~ }
 
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'password']
+    #~ # extra validation through django native
+    #~ def __init__(self, *args, **kwargs):
+        #~ super(RegisterForm, self).__init__(*args, **kwargs)
 
-        widgets = { 
-            'username': forms.TextInput(attrs={'placeholder': 'Username'}),
-            'password': forms.PasswordInput(attrs={'placeholder': 'Password'}),
-            'email': forms.EmailInput(attrs={'placeholder': 'E-mail address'}),
-            'first_name': forms.TextInput(attrs={'placeholder': 'first name'}),
-            'last_name': forms.TextInput(attrs={'placeholder': 'last name'}),
-         }
+        #~ for key in self.fields:
+            #~ self.fields[key].required = True
 
-    # extra validation through django native
-    def __init__(self, *args, **kwargs):
-        super(RegisterForm, self).__init__(*args, **kwargs)
+#~ # dynamic form for extended user model
+#~ class UserDataForm(forms.ModelForm):
 
-        for key in self.fields:
-            self.fields[key].required = True
+    #~ class Meta:
+        #~ model = User_data
+        #~ fields = ('phone_number','apartment')
 
+        #~ widgets = { 
+            #~ 'phone_number': forms.TextInput(attrs={'placeholder': 'Phone number'}),
+            #~ 'apartment': forms.TextInput(attrs={'placeholder': 'Apartment number'}),
+        #~ }
+        
+# cool register form
+class RegisterForm(forms.Form):
+    
+    alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Only the letters A to Z and numbers are allowed.')
+    
+    username = forms.CharField(
+        min_length=3,
+        validators=[alphanumeric],
+        widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+
+    first_name = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'First name'}))
+
+    last_name = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Last name'}))
+
+    email = forms.CharField(
+        widget=forms.EmailInput(attrs={'placeholder': 'E-mail address'}))
+
+    password = forms.CharField(
+        min_length=8,
+        widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+
+    password_repeat = forms.CharField(
+        min_length=8,
+        widget=forms.PasswordInput(attrs={'placeholder': 'Repeat password'}))
+
+    phone_number = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Phone number'}))
+
+    apartment = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Apartment number'}))
+    
+    def clean_username(self):
+        cleaned_username = self.cleaned_data['username']
+        
+        if User.objects.filter(username=cleaned_username).exists():
+            raise forms.ValidationError(u"The username %s is already taken." % cleaned_username)
+            
+        return cleaned_username
+
+    def clean_password_repeat(self):
+        #Check if the password was repeated, and if the repeated password is a match.
+        cleaned_password = self.cleaned_data.get('password')
+        cleaned_password_repeat = self.cleaned_data['password_repeat']
+
+        if not cleaned_password_repeat:
+            raise forms.ValidationError(u"Please repeat the password.")
+        if cleaned_password != cleaned_password_repeat:
+            raise forms.ValidationError(u"The passwords do not match.")
+            
+        return cleaned_password_repeat
+
+    def clean_apartment(self):
+        cleaned_apartment = self.cleaned_data['apartment']
+        
+        if User_data.objects.filter(apartment=cleaned_apartment).exists():
+            raise forms.ValidationError(u"This apartment is already in use.")
+            
+        if not Apartment_number.objects.filter(apartment_number=cleaned_apartment).exists():
+            raise forms.ValidationError(u"This apartment does not exist.")
+            
+        return cleaned_apartment
 
 # used for forgot password email view
 # remove if no longer required
--- parkering/account/views.py
+++ parkering/account/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import auth
 from account.models import User_data, Apartment_number
-from account.forms import UserDataForm, LoginForm, PasswordResetRequestForm, RegisterForm
+from account.forms import LoginForm, PasswordResetRequestForm, RegisterForm
 from django.contrib.auth import logout
 from django.core.mail import EmailMessage
 from django.contrib.auth.views import password_reset, password_reset_confirm
@@ -26,34 +26,6 @@ password_confirm = 'account/Password_reset_confirm.html'
 authentication_error = 'account/error/Not_authorized.html'
 # development only
 devtest = 'dev/test.html'
-
-### error messages ###
-
-# list of error messages, empty if no errors
-# always add ErrorMessages.clear() on initial use
-# to avoid old errors being displayed
-errorMessages = list()
-
-# incorrect login details
-incorrectDetails = "IncorrectDetails"
-# user exists
-userExists = "UserExists"
-# user exists
-passwordMismatch = "PasswordMismatch"
-# invalid registerForm
-invalidForm = "InvalidForm"
-# invalid apartment number
-invalidNumber = "InvalidNumber"
-# apartment number already in use
-numberInUse = "NumberInUse"
-# short username
-usernameLength = "UsernameLength"
-# short password
-passwordLength = "PasswordLength"
-# unexpected error
-unexpected = "Unexpected"
-
-### End error messages ###
 
 # class for handling index page functionality
 class Index:
@@ -99,119 +71,58 @@ class AccountManagement:
 class Register:
     # Render register screen
     def Register_screen(request):
-        errorMessages.clear()
-
-        context = {'DataForm': UserDataForm,'RegForm': RegisterForm, 'ErrorMessages': errorMessages }
+        
+        context = { 'RegForm': RegisterForm }
         return render(request, register, context)
 
     # Create new user
     def Register_account(request):
-        errorMessages.clear()
 
-        RegForm = RegisterForm(request.POST or None)
-        DataForm = UserDataForm(request.POST or None)
-        context = {'DataForm': UserDataForm,'RegForm': RegisterForm, 'ErrorMessages': errorMessages }
-
-        ### Validation ###
-
-        # true if errors
-        error = False
-
-        # form raw data for validation
-        username = RegForm.data['username']
-        password = RegForm.data['password']
-        apartment_number = DataForm.data['apartment']
-        repeat_password = request.POST.get('Repeat_password', '')
-
-        # check if username already exists and is not empty
-        if User.objects.filter(username=username).exists():
-            error = True
-            errorMessages.append(userExists)
-
-        # username minimum length
-        if len(username) < 3:
-            error = True
-            errorMessages.append(usernameLength)
-
-        # password minimum length
-        if len(password) < 4:
-            error = True
-            errorMessages.append(passwordLength)
-
-        # check if provided passwords match
-        if password != repeat_password:
-            error = True
-            errorMessages.append(passwordMismatch)
-
-        # check that the number is not currently in use
-        if User_data.objects.filter(apartment=apartment_number).exists():
-            error = True
-            errorMessages.append(numberInUse)
-
-        # check that the provided apartment exist in Apartment_number
-        if not Apartment_number.objects.filter(apartment_number=apartment_number).exists():
-            error = True
-            errorMessages.append(invalidNumber)
-
-        # re-render page with all the appended errors
-        if error:
-            return render(request, register, context)
-        else:
-            pass
-
-        ### End Validation ###
-
-        if request.POST and RegForm.is_valid() and DataForm.is_valid():
-            password = RegForm.cleaned_data['password']
-            email = RegForm.cleaned_data['email']
-            first_name = RegForm.cleaned_data['first_name']
-            last_name = RegForm.cleaned_data['last_name']
-            phone_number = DataForm.cleaned_data['phone_number']
-            apartment = DataForm.cleaned_data['apartment']
-            repeat_password = request.POST.get('Repeat_password', '')
+        register_form = RegisterForm(request.POST or None)
+        
+        if request.POST and register_form.is_valid():
+            password = register_form.cleaned_data['password']
+            email = register_form.cleaned_data['email']
+            first_name = register_form.cleaned_data['first_name']
+            last_name = register_form.cleaned_data['last_name']
+            phone_number = register_form.cleaned_data['phone_number']
+            apartment = register_form.cleaned_data['apartment']
 
             if request.user.is_authenticated():
                 return redirect('/test') # TODO: redirect to post-login page
             else:
                 user = User(username=username)
-                User_Data = User_data(user=user)
                 user.email = email
                 user.first_name = first_name
                 user.last_name = last_name
                 user.set_password(password)
                 user.is_active = True
                 user.save()
+                
                 User_Data = User_data(user=user)
                 User_Data.phone_number = phone_number
                 User_Data.apartment = apartment
                 User_Data.save()
+                
                 user = auth.authenticate(username = username, password = password)
+                
                 if user:
                     auth.login(request, user)
                     return redirect('/test') # TODO: redirect to post-login page
-                else:
-                    errorMessages.append(unexpected)
-                    return render(request, register, context)
 
-        # error messages to display
-        errorMessages.append(invalidForm)
-        return render(request, register, context)
+        return render(request, register, { 'RegForm': register_form })
 
 
 # class for handling login functionality
 class Login:
     # Render login screen
     def Login_screen(request):
-        errorMessages.clear()
 
-        context = {'LogForm': LoginForm, 'ErrorMessages': errorMessages }
+        context = {'LogForm': LoginForm }
         return render(request, login, context)
 
     # Log in for registered users
     def Login_check(request):
-        errorMessages.clear()
-
-        #~ context = { 'LogForm': LogForm, 'ErrorMessages': errorMessages }
 
         if request.method == 'POST':
             LogForm = LoginForm(request.POST)
@@ -223,16 +134,12 @@ class Login:
                 if user:
                     auth.login(request, user)
                     return redirect('/test') # TODO: redirect to post-login page
-                #~ else:
-                    #~ # if incorrect details
-                    #~ errorMessages.append(unexpected)
-                    #~ return render(request, login, context)
+                    
         else:
             LogForm = LoginForm()
 
         # if invalid form
-        errorMessages.append(incorrectDetails)
-        return render(request, login, { 'LogForm': LogForm, 'ErrorMessages': errorMessages })
+        return render(request, login, { 'LogForm': LogForm })
     
 
 # class for handling logout functionality
