6ef23225f7a284a9622a3426d2236def17be1dbe
Max <max.arnstrom@gmail.com>
2016-12-15 11:03:41 +0100

    removed unused code in account/forms

--- parkering/account/forms.py
+++ parkering/account/forms.py
@@ -12,25 +12,14 @@ minimum_password_length = 8
 # dynamic loginform
 class LoginForm(forms.Form):
     username = forms.CharField(
-        label=(""), 
         widget=forms.TextInput(attrs={'placeholder': 'Username'}))
 
     password = forms.CharField(
-        label=(""), 
         widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
 
     # remove when no longer required
     # access to this but no validation error is displayed in html, with or without tags
     # can be accessed by print(<form>.errors) in views.py
-    #~ def clean_password(self):
-        #~ cleaned_username = self.cleaned_data['username']
-        #~ cleaned_password = self.cleaned_data['password']
-
-        #~ user = auth.authenticate(username = cleaned_username, password = cleaned_password)
-        #~ if not user:
-            #~ raise forms.ValidationError(u"Incorrect login, please try again or register an account.")
-        #~ return cleaned_password
-
     def clean_password(self):
         cleaned_username = self.cleaned_data['username']
         cleaned_password = self.cleaned_data['password']
@@ -45,40 +34,6 @@ class LoginForm(forms.Form):
 
         return cleaned_password
         
-#~ # dynamic register form
-#~ class RegisterForm(forms.ModelForm):
-
-    #~ class Meta:
-        #~ model = User
-        #~ fields = ['username', 'email', 'first_name', 'last_name', 'password']
-
-        #~ widgets = { 
-            #~ 'username': forms.TextInput(attrs={'placeholder': 'Username'}),
-            #~ 'password': forms.PasswordInput(attrs={'placeholder': 'Password'}),
-            #~ 'email': forms.EmailInput(attrs={'placeholder': 'E-mail address'}),
-            #~ 'first_name': forms.TextInput(attrs={'placeholder': 'first name'}),
-            #~ 'last_name': forms.TextInput(attrs={'placeholder': 'last name'}),
-         #~ }
-
-    #~ # extra validation through django native
-    #~ def __init__(self, *args, **kwargs):
-        #~ super(RegisterForm, self).__init__(*args, **kwargs)
-
-        #~ for key in self.fields:
-            #~ self.fields[key].required = True
-
-#~ # dynamic form for extended user model
-#~ class UserDataForm(forms.ModelForm):
-
-    #~ class Meta:
-        #~ model = User_data
-        #~ fields = ('phone_number','apartment')
-
-        #~ widgets = { 
-            #~ 'phone_number': forms.TextInput(attrs={'placeholder': 'Phone number'}),
-            #~ 'apartment': forms.TextInput(attrs={'placeholder': 'Apartment number'}),
-        #~ }
-
 # cool register form
 class RegisterForm(forms.Form):
     
