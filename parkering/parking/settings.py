3be48c5e93395670188d8ae74c776926195f2929
ve2323 <kim.sillanpaa@hotmail.com>
2017-01-12 14:10:00 +0100

    script completion
    Completed script to remove all entries older than 31 days at:
    /scripts/clear_old_entries.py

--- parkering/parking/settings.py
+++ parkering/parking/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'django_extensions',
+    'django_extensions',
 ]
 
 MIDDLEWARE_CLASSES = [
--- parkering/scripts/clear_old_entries.py
+++ parkering/scripts/clear_old_entries.py
@@ -1,16 +1,17 @@
 from __future__ import unicode_literals
-from django.conf import settings
 from datetime import datetime, timedelta
-import dateutil.relativedelta
 from kombo_parking.models import Booking
 
 def run():
-    old_bookings = datetime.now() - dateutil.relativedelta.relativedelta(months=1)
-    bookings = Booking.objects.exclude(stop_date__gt=old_bookings)
+
+    # fetch all bookings with a stop date older than 31 days
+    bookings = Booking.objects.filter(stop_date__lte=datetime.now()-timedelta(days=31))
     
+    # delete all bookings from database if there are any
     if bookings.exists():
-    	for b in bookings:
-       		b.delete()
+        for b in bookings:
+            print("Deleted: ", b)
+            b.delete()
 
 
 
