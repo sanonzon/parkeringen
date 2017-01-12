816842fcda4fda0fb7595b78fbe89bf314d31422
ve2323 <kim.sillanpaa@hotmail.com>
2017-01-12 09:10:58 +0100

    navbar text and converted to english calendar

--- parkering/parking/settings.py
+++ parkering/parking/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 #SECURE_SSL_REDIRECT = True
 
 # allow host komboparking.pythonanywhere.com in release
-ALLOWED_HOSTS = ['komboparking.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 # Application definition
 
