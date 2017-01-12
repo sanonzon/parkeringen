773bef2f39553a085f4d45fa46056c60daaf5b91
ve2323 <kim.sillanpaa@hotmail.com>
2017-01-12 10:14:38 +0100

    script code update and calender booked modal text change
    yep.

--- parkering/scripts/clear_old_entries.py
+++ parkering/scripts/clear_old_entries.py
@@ -1,31 +1,16 @@
 from __future__ import unicode_literals
-#~ import os, sys
-#~ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-#~ import django
-#~ os.environ.setdefault("DJANGO_SETTING_MODUL", "parking.settings")
 from django.conf import settings
-
-
-#~ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from datetime import datetime, timedelta
 import dateutil.relativedelta
 from kombo_parking.models import Booking
 
 def run():
-    old_shit = datetime.now() - dateutil.relativedelta.relativedelta(months=1)
-    bokningar = Booking.objects.exclude(stop_date__gt=old_shit)
+    old_bookings = datetime.now() - dateutil.relativedelta.relativedelta(months=1)
+    bookings = Booking.objects.exclude(stop_date__gt=old_bookings)
     
-    for b in bokningar:
-        print(b.number)
-    
-    # print datetime.now()
-    # print d
-    
-    #~ doot = Booking.objects.filter(stop_date > datetime.now())
-    
-    #~ for d in doot:
-        #~ print d.number
-
+    if bookings.exists():
+    	for b in bookings:
+       		b.delete()
 
 
 
