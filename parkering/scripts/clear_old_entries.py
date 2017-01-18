2a836764d4e5bbeb7283dd2197399f8b60e3a2b4
Axel <pennsater@gmail.com>
2017-01-18 07:47:32 +0100

    Added Requested spaces to script file. Timedelta still 1 day.

--- parkering/scripts/clear_old_entries.py
+++ parkering/scripts/clear_old_entries.py
@@ -1,17 +1,22 @@
 from __future__ import unicode_literals
 from datetime import datetime, timedelta
-from kombo_parking.models import Booking
+from kombo_parking.models import Booking, Re
 
 def run():
 
     # fetch all bookings with a stop date older than 1 days
     bookings = Booking.objects.filter(stop_date__lte=datetime.now()-timedelta(days=1)
+    requests = Requested_Space.objects.filter(stop_date__lte=datetime.now()-timedelta(days=1)
     
     # delete all bookings from database if there are any
     if bookings.exists():
         for b in bookings:
             print("Deleted: ", b)
             b.delete()
-
-
+            
+    # delete all requests from database if there are any
+    if requests.exists():
+        for b in requests:
+            print("Deleted: ", b)
+            b.delete()
 
