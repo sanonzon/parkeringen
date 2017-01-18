6e1a0d955a66a79bb212fa961b7ef480f5e9ed76
Axel <pennsater@gmail.com>
2017-01-18 10:10:04 +0100

    Fixed delete_old_entries script, fixed Calendar, only possible view is a month at a time.

--- parkering/scripts/clear_old_entries.py
+++ parkering/scripts/clear_old_entries.py
@@ -1,12 +1,12 @@
 from __future__ import unicode_literals
 from datetime import datetime, timedelta
-from kombo_parking.models import Booking, Re
+from kombo_parking.models import Booking, Requested_Space
 
 def run():
 
     # fetch all bookings with a stop date older than 1 days
-    bookings = Booking.objects.filter(stop_date__lte=datetime.now()-timedelta(days=1)
-    requests = Requested_Space.objects.filter(stop_date__lte=datetime.now()-timedelta(days=1)
+    bookings = Booking.objects.filter(stop_date__lte=datetime.now()-timedelta(days=1))
+    requests = Requested_Space.objects.filter(stop_date__lte=datetime.now()-timedelta(days=1))
     
     # delete all bookings from database if there are any
     if bookings.exists():
