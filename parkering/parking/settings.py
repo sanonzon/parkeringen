d2657349ba342755fdbfc6d915a832a23a83d679
Axel <pennsater@gmail.com>
2017-02-02 11:51:38 +0100

    Merge gui_locale, still some templates left to implement translations.

--- parkering/parking/settings.py
+++ parkering/parking/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'flyyu)po656qy#z9iel9ac(v&1b-u&4w7yv$#np!k_(k58vb^1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.0.100','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['komboparking.pythonanywhere.com']
 
 
 # Application definition
